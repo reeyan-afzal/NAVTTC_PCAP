295f317f70010e546a8fc1f207a3eb30e5023445
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-07-24 17:39:35 +0500

    Finalizing Task 2

--- 24_07_24-Task2/main.py
+++ 24_07_24-Task2/main.py
@@ -2,12 +2,12 @@
 
 stdName = input("Enter your name: ")
 stdFName = input("Enter your father name: ")
-stdSub1Marks = float(input("Enter your marks in Subject1(100.0): "))
-stdSub2Marks = float(input("Enter your marks in Subject2(100.0): "))
+stdSub1Marks = float(input("Enter your marks in Subject-1(100.0): "))
+stdSub2Marks = float(input("Enter your marks in Subject-2(100.0): "))
 
-print("\n**************************************************")
+print('\n' + "*" * 50)
 print("Welcome! " + stdName + ", father name " + stdFName)
 print("You've got", stdSub1Marks, "in Subject 1 and", stdSub2Marks, "in Subject 2")
 print("You've secured", stdSub1Marks + stdSub2Marks, "marks out of 200")
 print("Your Percentage is: ", ((stdSub1Marks + stdSub2Marks) / 200) * 100)
-print("**************************************************\n")
+print("*" * 50, end='\n')

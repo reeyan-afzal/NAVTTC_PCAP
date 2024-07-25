51f7ed31d1310710d1e780a3c0ee1af84623ba14
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-07-25 14:40:56 +0500

    Fixing Task 2 - Absolute Path

--- 24_07_23-Homework1/main.py
+++ 24_07_23-Homework1/main.py
@@ -2,17 +2,20 @@
 # https://www.youtube.com/watch?v=HXDD7-EnGBY&ab_channel=Murtaza%27sWorkshop-RoboticsandAI
 
 import cv2
+import os
 
 cap = cv2.VideoCapture(0)  # 0 for native webcam, 1 for phone webcam
 cap.set(3, 640)
 cap.set(4, 480)
 
-classFile = 'Object_Detection_Files/coco.names'
-with open(classFile, 'rt') as f:
-    classNames = f.read().rstrip('\n').split('\n')
+classFile = os.getcwd() + "/Object_Detection_Files/coco.names"
+with open(classFile, "rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
 
-configPath = 'Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
-weightPath = 'Object_Detection_Files/frozen_inference_graph.pb'
+configPath = (
+    os.getcwd() + "/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+)
+weightPath = os.getcwd() + "/Object_Detection_Files/frozen_inference_graph.pb"
 
 net = cv2.dnn_DetectionModel(weightPath, configPath)
 net.setInputSize(320, 320)
@@ -29,12 +32,20 @@ try:
         classIds, confs, bbox = net.detect(img, confThreshold=0.5)
 
         for classId, confidence, box in zip(classIds, confs, bbox):
-            label = f'{classNames[classId - 1]}: {confidence:.2f}'
+            label = f"{classNames[classId - 1]}: {confidence:.2f}"
             cv2.rectangle(img, box, color=(0, 255, 0), thickness=2)
-            cv2.putText(img, label, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            cv2.putText(
+                img,
+                label,
+                (box[0], box[1]),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.5,
+                (0, 255, 0),
+                2,
+            )
 
-        cv2.imshow('Object Detection', img)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.imshow("Object Detection", img)
+        if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 except KeyboardInterrupt:
     print("Interrupted by user")

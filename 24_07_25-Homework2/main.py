768e91904f0960bd469b18a546688c0459d2c741
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-07-26 11:53:46 +0500

    Homework 2 - Removing Redundancy

--- 24_07_25-Homework2/main.py
+++ 24_07_25-Homework2/main.py
@@ -1,16 +1,6 @@
-print('*' * 37)
-print('| Welcome To My Telenor Offer Menu. |')
-print('*' * 37, end='\n')
+# Homework 2 - Telenor Package Management
 
-threeOptions = [1, 2, 3]
-fiveOptions = [1, 2, 3, 4, 5]
-
-package_duration_list = [1, 7, 30]
-
-minutes_mbs_duration_list = [0, 50, 100, 200, 500]
-
-sms_duration_list = [0, 100, 200, 300, 500]
-
+""" Package Rates"""
 VALIDITY_COST = [10, 50, 150]
 ONNET_COST_PER_MINUTE = 0.5
 OFFNET_COST_PER_MINUTE = 1.0
@@ -18,44 +8,48 @@ DATA_COST_PER_MB = 1.0
 SOCIAL_COST_PER_MB = 0.5
 SMS_COST_PER_UNIT = 0.1
 
+print('*' * 37)
+print('| Welcome To My Telenor Offer Menu. |')
+print('*' * 37, end='\n')
+
+package_duration_list = [1, 7, 30]
+minutes_mbs_package_list = [0, 50, 100, 200, 500]
+sms_package_list = [0, 100, 200, 300, 500]
+
 
 def package_duration():
-    print('\nPlease select Offer Validity.'
-          '\n1)', package_duration_list[0], 'Day',
-          '\n2)', package_duration_list[1], 'Days',
-          '\n3)', package_duration_list[2], 'Days')
-
-    return int(input('\nSelect: '))
-
-
-def minutes_mbs_offer(type1, type2):
-    print('\nPlease select ' + type1 + ' ' + type2,
-          '\n1)', minutes_mbs_duration_list[0], type2,
-          '\n2)', minutes_mbs_duration_list[1], type2,
-          '\n3)', minutes_mbs_duration_list[2], type2,
-          '\n4)', minutes_mbs_duration_list[3], type2,
-          '\n5)', minutes_mbs_duration_list[4], type2,
-          '\n0) Back')
-
-    selection = int(input('\nSelect: '))
-    if selection == 0:
-        return 'back'
-    return selection
+    print('\nPlease select Offer Validity')
+    for idx, days in enumerate(package_duration_list):
+        print(f"{idx + 1}) {days} Day{'s' if days > 1 else ''}")
+    try:
+        selection = int(input('\nSelect: '))
+        if selection == 0 or selection not in range(1, len(package_duration_list) + 1):
+            print("\nInvalid selection. Exiting program.")
+            exit()
+        return selection - 1
+    except ValueError:
+        print("\nInvalid input. Exiting program.")
+        exit()
 
 
-def sms_offer():
-    print('\nPlease select SMS.'
-          '\n1)', sms_duration_list[0], 'SMS',
-          '\n2)', sms_duration_list[1], 'SMS',
-          '\n3)', sms_duration_list[2], 'SMS',
-          '\n4)', sms_duration_list[3], 'SMS',
-          '\n5)', sms_duration_list[4], 'SMS',
-          '\n0) Back')
+def package_offer(package_list, type1, type2):
+    print(f'\nPlease select{type1} {type2}')
+    for idx, package in enumerate(package_list):
+        print(f"{idx + 1}) {package} {type2}")
+    print('0) Back')
 
-    selection = int(input('\nSelect: '))
-    if selection == 0:
-        return 'back'
-    return selection
+    try:
+        selection = int(input('\nSelect: '))
+        if selection == 0:
+            return 'back'
+        elif selection in range(1, len(package_list) + 1):
+            return selection - 1
+        else:
+            print("\nInvalid selection. Exiting program.")
+            exit()
+    except ValueError:
+        print("\nInvalid input. Exiting program.")
+        exit()
 
 
 def calculate_package_cost(validity, onnet_minutes, offnet_minutes, data_mbs, social_mbs, sms):
@@ -75,100 +69,92 @@ def package_selected(user_package_duration,
                      user_social_mbs,
                      user_sms):
     selected_items = []
-    counter = 1
 
-    if user_sms != 0:
-        selected_items.append(f"{counter}) {sms_duration_list[user_sms]}")
-        counter += 1
+    def add_item(value, label):
+        if value != 0:
+            selected_items.append(f"{len(selected_items) + 1}) {value} {label}")
 
-    if user_social_mbs != 0:
-        selected_items.append(f"{counter}) {minutes_mbs_duration_list[user_social_mbs]} Social MBs")
-        counter += 1
+    add_item(sms_package_list[user_sms], "SMS")
+    add_item(minutes_mbs_package_list[user_social_mbs], "Social MBs")
+    add_item(minutes_mbs_package_list[user_data_mbs], "Data MBs")
+    add_item(minutes_mbs_package_list[user_offnet_minutes], "Offnet Minutes")
+    add_item(minutes_mbs_package_list[user_onnet_minutes], "Onnet Minutes")
 
-    if user_data_mbs != 0:
-        selected_items.append(f"{counter}) {minutes_mbs_duration_list[user_data_mbs]} Data MBs")
-        counter += 1
-
-    if user_offnet_minutes != 0:
-        selected_items.append(f"{counter}) {minutes_mbs_duration_list[user_offnet_minutes]} Offnet Minutes")
-        counter += 1
-
-    if user_onnet_minutes != 0:
-        selected_items.append(f"{counter}) {minutes_mbs_duration_list[user_onnet_minutes]} Onnet Minutes")
-        counter += 1
-
-    selected_items.append(f"Validity: {package_duration_list[user_package_duration]}")
+    selected_items.append(f"Validity: {package_duration_list[user_package_duration]} days")
 
     total_cost = calculate_package_cost(user_package_duration,
-                                        minutes_mbs_duration_list[user_onnet_minutes],
-                                        minutes_mbs_duration_list[user_offnet_minutes],
-                                        minutes_mbs_duration_list[user_data_mbs],
-                                        minutes_mbs_duration_list[user_social_mbs],
-                                        sms_duration_list[user_sms])
+                                        minutes_mbs_package_list[user_onnet_minutes],
+                                        minutes_mbs_package_list[user_offnet_minutes],
+                                        minutes_mbs_package_list[user_data_mbs],
+                                        minutes_mbs_package_list[user_social_mbs],
+                                        sms_package_list[user_sms])
 
     print(f'\nSirf Rs {total_cost:.2f} bama tax main hasil karen:')
     for item in selected_items:
         print(item)
 
-    print("\n1) Activate\n2) Cancel")
+    print("\n1) Activate\n2) Home\n3) Cancel")
 
-    _final_choice = int(input('\nSelect: '))
-    print("\nThank you for activating the current package.\nGood Bye!") if _final_choice == 1 else exit()
+    try:
+        _final_choice = int(input('\nSelect: '))
+        if _final_choice == 1:
+            print("\nThank you for activating the current package.\nGood Bye!")
+            exit()
+        elif _final_choice == 2:
+            return True
+        elif _final_choice == 3:
+            exit()
+        else:
+            print("\nInvalid selection. Exiting program.")
+            exit()
+    except ValueError:
+        print("\nInvalid input. Exiting program.")
+        exit()
+
+
+def get_user_selected_package(package_list, label1, label2):
+    while True:
+        user_choice = package_offer(package_list, label1, label2)
+        if user_choice == 'back' or user_choice in range(len(package_list)):
+            return user_choice
+
+
+def package_selection():
+    steps = [
+        (" Onnet", "Minutes", minutes_mbs_package_list),
+        (" Offnet", "Minutes", minutes_mbs_package_list),
+        (" Data", "MBs", minutes_mbs_package_list),
+        (" Social", "MBs", minutes_mbs_package_list),
+        ("", "SMS", sms_package_list)
+    ]
+
+    while True:
+        _userPackageDuration = package_duration()
+        if _userPackageDuration not in range(len(package_duration_list)):
+            return
+
+        user_choices = [-1] * len(steps)
+        current_step = 0
+
+        while current_step < len(steps):
+            label1, label2, package_list = steps[current_step]
+            user_choice = get_user_selected_package(package_list, label1, label2)
+            if user_choice == 'back':
+                if current_step > 0:
+                    current_step -= 1
+                else:
+                    return
+            else:
+                user_choices[current_step] = user_choice
+                current_step += 1
+
+        if not package_selected(_userPackageDuration, *user_choices):
+            continue
 
 
 try:
     while True:
-        _userPackageDuration = package_duration()
-
-        if _userPackageDuration in threeOptions:
-            while True:
-                _userOnnetMinutes = minutes_mbs_offer("Onnet", "Minutes")
-                if _userOnnetMinutes == 'back':
-                    break
-
-                if _userOnnetMinutes in fiveOptions:
-                    while True:
-                        _userOffnetMinutes = minutes_mbs_offer("Offnet", "Minutes")
-                        if _userOffnetMinutes == 'back':
-                            break
-
-                        if _userOffnetMinutes in fiveOptions:
-                            while True:
-                                _userDataMBs = minutes_mbs_offer("Data", "MBs")
-                                if _userDataMBs == 'back':
-                                    break
-
-                                if _userDataMBs in fiveOptions:
-                                    while True:
-                                        _userSocialMBs = minutes_mbs_offer("Social", "MBs")
-                                        if _userSocialMBs == 'back':
-                                            break
-
-                                        if _userSocialMBs in fiveOptions:
-                                            while True:
-                                                _userSMS = sms_offer()
-                                                if _userSMS == 'back':
-                                                    break
-
-                                                if _userSMS in fiveOptions:
-                                                    package_selected(_userPackageDuration - 1,
-                                                                     _userOnnetMinutes - 1,
-                                                                     _userOffnetMinutes - 1,
-                                                                     _userDataMBs - 1,
-                                                                     _userSocialMBs - 1,
-                                                                     _userSMS - 1)
-                                                    exit()
-                                                elif _userSMS == '0':
-                                                    break
-                                        elif _userSocialMBs == '0':
-                                            break
-                                elif _userDataMBs == '0':
-                                    break
-                        elif _userOffnetMinutes == '0':
-                            break
-                elif _userOnnetMinutes == '0':
-                    break
-        else:
-            exit()
+        package_selection()
 except (KeyboardInterrupt, ValueError) as e:
+    print("\nProgram terminated.")
     exit()

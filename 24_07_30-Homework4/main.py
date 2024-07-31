0cde33b6cbaf66da9d7b8d43d31c5f65527a8ae4
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-07-31 10:06:11 +0500

    Fixing Homework 4

--- 24_07_30-Homework4/main.py
+++ 24_07_30-Homework4/main.py
@@ -24,9 +24,7 @@ try:
             break
 
         plate_cascade = cv2.CascadeClassifier(harcascade)
-
         img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
         plates = plate_cascade.detectMultiScale(img_gray, 1.1, 4)
 
         for (x, y, w, h) in plates:
@@ -34,13 +32,9 @@ try:
 
             if area > min_area:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
                 img_roi = img[y: y + h, x:x + w]
-
                 text = pytesseract.image_to_string(img_roi, config='--psm 7')
-
                 cv2.putText(img, text.strip(), (x, y - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2)
-
                 cv2.imshow("ROI", img_roi)
 
         cv2.imshow("Result", img)
@@ -50,7 +44,7 @@ try:
             save_path = os.path.join(os.getcwd(), "plates", f"scanned_img_{count}.jpg")
             cv2.imwrite(save_path, img_roi)
             cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+            cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 1)
             cv2.imshow("Results", img)
             cv2.waitKey(500)
             count += 1

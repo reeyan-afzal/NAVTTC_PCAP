fcb16b09035dd4b3290fd4611d89bd7d8a444cdd
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-01 14:34:53 +0500

    Finishing Task 5 - Event End Time Calculator

--- 24_07_31-Task5/main.py
+++ 24_07_31-Task5/main.py
@@ -1,19 +1,22 @@
 # Task 5 - Event End Time Calculator
 
-def calculate_end_time(hrs, mins, durs):
-    total_mins = mins + durs
-    new_hrs = (hrs + total_mins // 60) % 24
+try:
+    while True:
+        start_hours = int(input("Enter the starting hour (0-23): "))
+        start_minutes = int(input("Enter the starting minutes (0-59): "))
+        duration_minutes = int(input("Enter the duration in minutes: "))
+
+        if 0 < start_hours < 23 and 0 < start_minutes < 59:
+            break
+        else:
+            print("\nInvalid Values!\n")
+
+    total_mins = start_minutes + duration_minutes
+    new_hrs = (start_hours + total_mins // 60) % 24
     new_mins = total_mins % 60
 
-    return new_hrs, new_mins
+    print(f"\nEvent will starts at: {start_hours:02}:{start_minutes:02}")
+    print(f"Event will end at:    {new_hrs:02}:{new_mins:02}")
 
-
-start_hours = int(input("Enter the starting hour (0-23): "))
-start_minutes = int(input("Enter the starting minutes (0-59): "))
-duration_minutes = int(input("Enter the duration in minutes: "))
-
-end_hours, end_minutes = calculate_end_time(start_hours, start_minutes, duration_minutes)
-
-print(f"\nEvent will starts at: {start_hours:02}:{start_minutes:02}")
-print(f"Event will end at:    {end_hours:02}:{end_minutes:02}")
-
+except KeyboardInterrupt:
+    exit()

87847e0cbae4aa7bf38d3be67d4afa14ec17c1ae
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-08 16:37:31 +0500

    Updating README.md

--- 24_08_08-Task20/main-Task20.py
+++ 24_08_08-Task20/main-Task20.py
@@ -7,12 +7,24 @@ print(list3)
 del list3[0]
 print(list1)
 
-
 list_1 = [1, 3, 6, 7, 8, 9]
 list_2 = list_1[:]
 list_1[0] = 2
 print(list_2)
 
 my_list = [10, 8, 6, 4, 2]
-new_list = my_list[1:3]
+new_list = my_list[1:3:2]
 print(new_list)
+
+print(my_list[-3:-1])
+print(my_list[-4::1])
+print(my_list[-4::2])
+print(my_list[::-2])
+print(my_list[-1::])
+
+print(my_list[0:len(my_list)])
+
+my_list1 = [1, 2, 3, 4, 5, 6]
+print(my_list1)
+del my_list1[4::]
+print(my_list1)

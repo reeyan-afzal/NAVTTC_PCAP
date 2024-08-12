b4354fd524276fcde8107a9d0cca4b4185b5a1fe
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-12 13:38:25 +0500

    Finishing Task 22

--- 24_08_12-Task22/main-Task22.py
+++ 24_08_12-Task22/main-Task22.py
@@ -30,7 +30,7 @@ ages = np.random.normal(20, 7.5, 1000)
 plt.hist(ages, bins=[ages.min(), 18, 21, ages.max()], cumulative=True)
 """
 
-# PieChart
+# Pie Chart
 """
 langs = ["Python", "C++", "C#", "Java", "Go"]
 votes = [50, 24, 6, 14, 17]
@@ -46,7 +46,7 @@ plt.pie(
 )
 """
 
-# BoxPlots
+# Box Plots
 heights = np.random.normal(172, 8, 300)
 
 plt.boxplot(heights)

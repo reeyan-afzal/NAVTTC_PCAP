0950db88bc11aef476bd1c2377d6fd283115945f
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-13 16:50:13 +0500

    Finishing Task 23

---
+++ 24_08_13-Task23/main.py
@@ -0,0 +1,20 @@
+# Finding the Leap Year
+
+def is_year_leap(year):
+    if year % 4 != 0 and year % 400 == 0:
+        return False
+    else:
+        return True
+
+
+test_data = [1990, 2000, 2016, 1987]
+test_results = [False, True, True, False]
+
+for i in range(len(test_data)):
+    yr = test_data[i]
+    print(yr, "-> ", end="")
+    result = is_year_leap(yr)
+    if result == test_results[i]:
+        print("OK")
+    else:
+        print("Failed")

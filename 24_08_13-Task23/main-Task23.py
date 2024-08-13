a543e8d232e22775e23780dad0ea78909a300d77
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-13 17:43:28 +0500

    Finishing Task 23

--- 24_08_13-Task23/main-Task23.py
+++ 24_08_13-Task23/main-Task23.py
@@ -1,19 +1,32 @@
 # Finding the Leap Year
 
 def is_year_leap(year):
-    if year % 4 != 0 and year % 400 == 0:
-        return False
-    else:
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         return True
+    else:
+        return False
+
+
+def days_in_month(year, month):
+    if not (1 <= month <= 12):
+        return None
+
+    days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if month == 2 and is_year_leap(year):
+        return 29
+    return days[month - 1]
 
 
 test_data = [1990, 2000, 2016, 1987]
-test_results = [False, True, True, False]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
 
 for i in range(len(test_data)):
     yr = test_data[i]
-    print(yr, "-> ", end="")
-    result = is_year_leap(yr)
+    mo = test_months[i]
+    print(yr, mo, "-> ", end="")
+    result = days_in_month(yr, mo)
     if result == test_results[i]:
         print("OK")
     else:

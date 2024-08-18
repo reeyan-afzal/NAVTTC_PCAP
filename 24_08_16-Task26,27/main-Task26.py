d5f06c859d79cf39d51b65c16850c343e32b2607
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-18 18:48:24 +0500

    Finishing Task 26 and 27

--- 24_08_16-Task26,27/main-Task26.py
+++ 24_08_16-Task26,27/main-Task26.py
@@ -1,1 +1,26 @@
 # Exploring Tuples and Dictionaries
+
+dict_1 = {"name": "Reeyan Afzal",
+          "email": "reeyanafzal1999@gmail.com",
+          "products": ["product_x", "product_y", "product_z"]
+          }
+
+dict_2 = {"products": ["product_x", "product_y", "product_z", "product_w"]}
+
+print(dict_1.keys())
+print(dict_1.values())
+print(dict_1.items())
+
+print()
+
+for product in dict_1['products']:
+    print(product)
+
+print()
+
+dict_1.update(dict_2)
+
+print()
+
+dict_1["products"].pop()
+print(dict_1)
--- 24_08_16-Task26,27/main-Task27.py
+++ 24_08_16-Task26,27/main-Task27.py
@@ -1,1 +1,19 @@
 # Exception Handling
+
+
+while True:
+    try:
+        num1 = float(input("Enter the nominator: "))
+        num2 = float(input("Enter the denominator: "))
+
+        print(f"{num1} divided by {num2} is: {num1 / num2}")
+
+    except ValueError:
+        print("Are you sure that's a number you're trying to enter!")
+
+    except ZeroDivisionError:
+        print("Seems like your math is terrible!")
+
+    finally:
+        print("\nProgram Finished! Go Home now!")
+        exit()

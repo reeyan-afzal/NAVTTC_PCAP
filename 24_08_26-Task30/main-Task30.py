cc0a554e57cad2cc868062111724a0f83d05a3a4
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-26 20:42:59 +0500

    Finishing Task 30 - String Manipulation

--- 24_08_26-Task30/main-Task30.py
+++ 24_08_26-Task30/main-Task30.py
@@ -21,25 +21,38 @@ try:
         print("_user_input_list:", list(_user_input_list))
 
         print()
-        print(f"The maximum in '{_user_input}' is {max(_user_input_list)}, corresponding to letter "
+        print(f"The maximum in '{_user_input}' is {max(_user_input_list)}, corresponding to letter",
               f"{chr(max(_user_input_list))}")
-        print(f"The minimum in '{_user_input}' is {min(_user_input_list)}, corresponding to letter "
-              f"{chr(min(_user_input_list))}")
+        print(f"The minimum in '{_user_input}' is {min(_user_input_list)}, corresponding to letter",
+              f"{chr(min(_user_input_list))}" if chr(min(_user_input_list)) == 32 else "'empty string'")
 
         half_index = len(_user_input) // 2
         first_half = _user_input[:half_index]
         second_half = _user_input[half_index:]
 
         print()
-        print(f"The first_half of '{_user_input}' is:", first_half)
-        print(f"The second_half of '{_user_input}' is:", second_half)
+        print(f"The first half of '{_user_input}' is: ", first_half)
+        print(f"The second half of '{_user_input}' is:", second_half)
 
         print()
         try:
-            print(_user_input.index('a'))
+            print(f"The index of 'a' in '{_user_input}' is:", _user_input.index('a'))
         except ValueError:
             print("The letter 'a' is not present in the string")
+
+        symbol = "+*"
+        design_length = max(30, len(_user_input) + 10)
+        border_line = (symbol * (design_length // len(symbol)))[:design_length]
+
+        centered_input = _user_input.center(design_length)
+
+        main_variable = f"""
+        {border_line}
+        {centered_input}
+        {border_line}
+        """
+
+        print(main_variable)
         print()
-        print("*" * 40, end='\n')
 except KeyboardInterrupt:
     exit()

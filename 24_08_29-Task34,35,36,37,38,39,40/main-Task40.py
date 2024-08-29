d37370ea3807c5ba127a456ed37b56d8de1916a5
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-29 18:48:55 +0500

    Finishing Task 40

--- 24_08_29-Task34,35,36,37,38,39,40/main-Task40.py
+++ 24_08_29-Task34,35,36,37,38,39,40/main-Task40.py
@@ -1,1 +1,12 @@
 # Task 40 - The Digit of Life
+
+def digit_of_life(digit):
+    while len(digit) > 1:
+        add_digits = sum(int(e) for e in digit)
+        digit = str(add_digits)
+    return int(digit)
+
+
+if __name__ == "__main__":
+    _digit = input("Enter the YYYYMMDD: ")
+    print(digit_of_life(list(_digit.strip())))

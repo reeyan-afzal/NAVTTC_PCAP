bc983fa1a1618b045f61c83fb2c1d31b5d72e06e
Reeyan Afzal <reeyanafzal1999@gmail.com>
2024-08-29 16:55:10 +0500

    Updating README.md

--- 24_08_29-Task34,35,36,37/main-Task37.py
+++ 24_08_29-Task34,35,36,37/main-Task37.py
@@ -17,6 +17,25 @@ def caesar_cipher(text, shift_value):
     return cipher
 
 
+def caesar_dicipher(text, shift_value):
+    cipher = ''
+    shift_value %= 26
+    for char in text:
+        if char.isalpha():
+            if char.islower():
+                base = ord('a')
+            else:
+                base = ord('A')
+
+            new_char = chr((ord(char) - base - shift_value) % 26 + base)
+            cipher += new_char
+        else:
+            cipher += char
+    return cipher
+
+
 if __name__ == "__main__":
     _text = input("Enter the text you want to cipher: ")
-    print("\nCiphered Text:", caesar_cipher(_text, 2))
+    _shift_value = int(input("Enter how much do you want to shift: "))
+    print("\nCiphered Text:", caesar_cipher(_text, _shift_value))
+    print("Deciphered Text:", caesar_dicipher(caesar_cipher(_text, _shift_value), _shift_value))
